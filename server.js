30e7a7d6a4066f1c46d5058ea4d7317c302bca25
rvmcc02-ctrl <rvmcc02@gmail.com>
2026-09-10 18:05:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set("trust proxy", true);
 const PORT = process.env.PORT || 3000;
 
 const distPath = path.join(__dirname, "dist");
